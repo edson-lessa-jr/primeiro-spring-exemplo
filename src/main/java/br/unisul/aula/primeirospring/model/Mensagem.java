a350b3df39c117194bffc8c0a22af442a575ffa8
Edson Lessa <edsonlessajunior@gmail.com>
2021-08-23 21:29:47 -0300

    Removido o loombok

--- src/main/java/br/unisul/aula/primeirospring/model/Mensagem.java
+++ src/main/java/br/unisul/aula/primeirospring/model/Mensagem.java
@@ -1,12 +1,40 @@
 package br.unisul.aula.primeirospring.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data @NoArgsConstructor @AllArgsConstructor
 public class Mensagem {
     private int codigo;
     private String titulo;
     private String mensagem;
+
+    public Mensagem() {
+    }
+
+    public Mensagem(int codigo, String titulo, String mensagem) {
+        this.codigo = codigo;
+        this.titulo = titulo;
+        this.mensagem = mensagem;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getMensagem() {
+        return mensagem;
+    }
+
+    public void setMensagem(String mensagem) {
+        this.mensagem = mensagem;
+    }
 }
